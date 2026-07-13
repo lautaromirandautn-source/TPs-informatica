121d4bcdffea92178c68ae331d3895a44584210e
Lauti <lautaromiranda.utn@gmail.com>
2026-07-13 00:54:16 -0300

    TP3: Corregir estructura del if y validaciones de los negativos

---
+++ Calculadora de IMC.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    float peso;
+    float altura;
+    float promedio;
+
+    do {
+        printf("Ingrese su peso en Kg: ");
+        scanf("%f", &peso);
+
+        if (peso < 0) {
+            printf("Error: el peso no puede ser negativo. Intente nuevamente.\n");
+        }
+    } while (peso < 0);
+
+    do {
+        printf("Ingrese su altura en metros: ");
+        scanf("%f", &altura);
+
+        if (altura < 0) {
+            printf("Error: la altura no puede ser negativa. Intente nuevamente.\n");
+        }
+    } while (altura < 0);
+
+    promedio = peso / (altura * altura);
+
+    printf("\n SU IMC ES DE: ");
+    printf("%.1f", promedio);
+    printf("\n    Indice    |  Condicion\n");
+    printf("-----------------------------\n");
+    printf("    <18.5     |  Bajo peso\n");
+    printf(" 18.5 a 24.9  |  Normal\n");
+    printf(" 25.0 a 29.9  |  Sobrepeso\n");
+    printf("     >=30     |  Obesidad\n");
+    if (promedio > 30)
+
+    //https://github.com/lautaromirandautn-source/TPs-informatica
+    return 0;
+}
